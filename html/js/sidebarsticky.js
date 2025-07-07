9e33ccb74c677726bc20f7123cf168d100b1bf8e
MuraliJbk <murali@jbkinfotech.com>
2025-07-07 19:13:16 +0530

    test

--- html/js/sidebarsticky.js
+++ html/js/sidebarsticky.js
@@ -1,15 +1,20 @@
 (() => {
   const __h = jQuery("header").height();
   const __ele = jQuery(".sticky-widget");
-  //   let jQuery(".services-banner-section").next().addClass("target-sec");
+  const nextele = jQuery(".services-banner-section")
+    .next()
+    .addClass("target-sec");
+  const targetele = nextele[0];
+
+  console.log(targetele);
 
   __ele.sticksy({ topSpacing: __h, listen: true });
   const callback = (entries) => {
     const [entry] = entries;
-    if (!entry.isIntersecting) {
-      jQuery(".services-banner-section").addClass("ha");
+    if (entry.isIntersecting) {
+      jQuery(".services-banner-section").addClass("remove-sticky-image");
     } else {
-      jQuery(".services-banner-section").removeClass("ha");
+      jQuery(".services-banner-section").removeClass("remove-sticky-image");
     }
   };
   const __observer = new IntersectionObserver(callback, {
@@ -17,5 +22,5 @@
     threshold: 0,
     rootMargin: "0px",
   });
-  __observer.observe(document.querySelector(".services-banner-section"));
+  __observer.observe(targetele);
 })();
